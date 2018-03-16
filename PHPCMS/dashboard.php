ee7b98cc1356c30c1db9d2a45f3c64dbefa64337
Ayush Acharya <aus.acharya@gmail.com>
2018-03-16 11:22:46 +0545

    Dash board created

---
+++ PHPCMS/dashboard.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/adminstyle.css">
+</head>
+<body>
+<div class="container-fluid">
+    <div class="row">
+
+        <div class="col-sm-2">
+            <h1>Ayush</h1>
+            <ul id="side_menu" class="nav nav-pills nav-stacked">
+                <li class="active"><a href="Dashboard.php">
+                        <span class="glyphicon glyphicon-th"></span>
+                        &nbsp;DashBoard</a></li>
+                <li><a href="#">
+                        <span class="glyphicon glyphicon-list-alt"></span>
+                        &nbsp;Add New Post</a> </li>
+                <li><a href="#">
+                        <span class="glyphicon glyphicon-tags"></span>
+                        &nbsp;Categories</a> </li>
+                <li><a href="#">
+                        <span class="glyphicon glyphicon-user"></span>
+                        &nbsp;Manage Admin</a> </li>
+                <li><a href="#">
+                        <span class="glyphicon glyphicon-comment"></span>
+                        &nbsp;Comments</a> </li>
+                <li><a href="#">
+                        <span class="glyphicon glyphicon-equalizer"></span>
+                        &nbsp;Live Blog</a> </li>
+                <li><a href="#">
+                        <span class="glyphicon glyphicon-log-out"></span>
+                        &nbsp;Logout</a> </li>
+            </ul>
+
+
+        </div> <!-- Ending of side srea -->
+        <div class="col-sm-10">
+            <h1>Admin Dashboard</h1>
+            <h4>About</h4>
+            <p>af dfidf <em>djfl sdkfjf fj efefj</em>
+            is a dfkj osdifj oko d fjdkf dsjeofw fpf oefj
+            f f of df f
+            fdsdfoo fj ef
+             fjj fj .</p>
+            <h4>About</h4>
+            <p>af dfidf <em>djfl sdkfjf fj efefj</em>
+                is a dfkj osdifj oko d fjdkf dsjeofw fpf oefj
+                f f of df f
+                fdsdfoo fj ef
+                fjj fj .</p><h4>About</h4>
+            <p>af dfidf <em>djfl sdkfjf fj efefj</em>
+                is a dfkj osdifj oko d fjdkf dsjeofw fpf oefj
+                f f of df f
+                fdsdfoo fj ef
+                fjj fj .</p><h4>About</h4>
+            <p>af dfidf <em>djfl sdkfjf fj efefj</em>
+                is a dfkj osdifj oko d fjdkf dsjeofw fpf oefj
+                f f of df f
+                fdsdfoo fj ef
+                fjj fj .</p><h4>About</h4>
+            <p>af dfidf <em>djfl sdkfjf fj efefj</em>
+                is a dfkj osdifj oko d fjdkf dsjeofw fpf oefj
+                f f of df f
+                fdsdfoo fj ef
+                fjj fj .</p><h4>About</h4>
+            <p>af dfidf <em>djfl sdkfjf fj efefj</em>
+                is a dfkj osdifj oko d fjdkf dsjeofw fpf oefj
+                f f of df f
+                fdsdfoo fj ef
+                fjj fj .</p><h4>About</h4>
+            <p>af dfidf <em>djfl sdkfjf fj efefj</em>
+                is a dfkj osdifj oko d fjdkf dsjeofw fpf oefj
+                f f of df f
+                fdsdfoo fj ef
+                fjj fj .</p><h4>About</h4>
+            <p>af dfidf <em>djfl sdkfjf fj efefj</em>
+                is a dfkj osdifj oko d fjdkf dsjeofw fpf oefj
+                f f of df f
+                fdsdfoo fj ef
+                fjj fj .</p><h4>About</h4>
+            <p>af dfidf <em>djfl sdkfjf fj efefj</em>
+                is a dfkj osdifj oko d fjdkf dsjeofw fpf oefj
+                f f of df f
+                fdsdfoo fj ef
+                fjj fj .</p><h4>About</h4>
+            <p>af dfidf <em>djfl sdkfjf fj efefj</em>
+                is a dfkj osdifj oko d fjdkf dsjeofw fpf oefj
+                f f of df f
+                fdsdfoo fj ef
+                fjj fj .</p><h4>About</h4>
+            <p>af dfidf <em>djfl sdkfjf fj efefj</em>
+                is a dfkj osdifj oko d fjdkf dsjeofw fpf oefj
+                f f of df f
+                fdsdfoo fj ef
+                fjj fj .</p><h4>About</h4>
+            <p>af dfidf <em>djfl sdkfjf fj efefj</em>
+                is a dfkj osdifj oko d fjdkf dsjeofw fpf oefj
+                f f of df f
+                fdsdfoo fj ef
+                fjj fj .</p><h4>About</h4>
+            <p>af dfidf <em>djfl sdkfjf fj efefj</em>
+                is a dfkj osdifj oko d fjdkf dsjeofw fpf oefj
+                f f of df f
+                fdsdfoo fj ef
+                fjj fj .</p><h4>About</h4>
+            <p>af dfidf <em>djfl sdkfjf fj efefj</em>
+                is a dfkj osdifj oko d fjdkf dsjeofw fpf oefj
+                f f of df f
+                fdsdfoo fj ef
+                fjj fj .</p><h4>About</h4>
+            <p>af dfidf <em>djfl sdkfjf fj efefj</em>
+                is a dfkj osdifj oko d fjdkf dsjeofw fpf oefj
+                f f of df f
+                fdsdfoo fj ef
+                fjj fj .</p><h4>About</h4>
+            <p>af dfidf <em>djfl sdkfjf fj efefj</em>
+                is a dfkj osdifj oko d fjdkf dsjeofw fpf oefj
+                f f of df f
+                fdsdfoo fj ef
+                fjj fj .</p><h4>About</h4>
+            <p>af dfidf <em>djfl sdkfjf fj efefj</em>
+                is a dfkj osdifj oko d fjdkf dsjeofw fpf oefj
+                f f of df f
+                fdsdfoo fj ef
+                fjj fj .</p><h4>About</h4>
+            <p>af dfidf <em>djfl sdkfjf fj efefj</em>
+                is a dfkj osdifj oko d fjdkf dsjeofw fpf oefj
+                f f of df f
+                fdsdfoo fj ef
+                fjj fj .</p><h4>About</h4>
+            <p>af dfidf <em>djfl sdkfjf fj efefj</em>
+                is a dfkj osdifj oko d fjdkf dsjeofw fpf oefj
+                f f of df f
+                fdsdfoo fj ef
+                fjj fj .</p>
+        </div><!-- ending of main area -->
+    </div><!-- ending of row -->
+</div><!-- ending of container fluid  -->
+</body>
+</html>
