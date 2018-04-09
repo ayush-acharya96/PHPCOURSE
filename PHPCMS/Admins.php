482bc32ab1b3dbc581f88a851c28f4ca90caa936
Ayush Acharya <aus.acharya@gmail.com>
2018-04-09 22:32:16 +0545

    admin registration added

--- PHPCMS/Admins.php
+++ PHPCMS/Admins.php
@@ -9,29 +9,33 @@
 ?>
 <?php
 if(isset($_POST["Submit"])){
-    $Category = mysql_real_escape_string($_POST["Category"]);
+    $Username = mysql_real_escape_string($_POST["Username"]);
+    $Password = mysql_real_escape_string($_POST["Password"]);
+    $ConfirmPassword = mysql_real_escape_string($_POST["ConfirmPassword"]);
     date_default_timezone_set('Asia/Kathmandu');
     $CurrentTime = time();
     $DateTime = strftime("%B-%d-%Y %H:%M:%S",$CurrentTime);
     $DateTime;
     $Admin = "Ayush Acharya";
-    if(empty($Category)){
+    if(empty($Username)||empty($Password)||empty($ConfirmPassword)){
         $_SESSION["ErrorMessage"] = "All fields must be filed out.";
-        Redirect_to("Categories.php");
-    } elseif(strlen($Category) > 99){
-        $_SESSION["ErrorMessage"] = "Too long Name.";
-        Redirect_to("Categories.php");
+        Redirect_to("Admins.php");
+    } elseif(strlen($Password) < 4) {
+        $_SESSION["ErrorMessage"] = "Password must be at least 4 characters.";
+        Redirect_to("Admins.php");
+    } elseif($Password !== $ConfirmPassword) {
+        $_SESSION["ErrorMessage"] = "Passwords donot match";
     } else {
         global $ConnectingDB;
-        $Query = "INSERT INTO category(datetime,name,creatorname)
-                  VALUES('$DateTime','$Category','$Admin')";
+        $Query = "INSERT INTO registration(datetime,username,password,addedby)
+                  VALUES('$DateTime','$Username','$Password','$Admin')";
         $Execute = mysql_query($Query);
         if($Execute) {
-            $_SESSION["SuccessMessage"] = "Category added successfully.";
-            Redirect_to("Categories.php");
+            $_SESSION["SuccessMessage"] = "Admin added successfully.";
+            Redirect_to("Admins.php");
         } else {
-            $_SESSION["ErrorMessage"] = "Category failed to add.";
-            Redirect_to("Categories.php");
+            $_SESSION["ErrorMessage"] = "Failed to add admin.";
+            Redirect_to("Admins.php");
         }
     }
 }
@@ -40,7 +44,7 @@ if(isset($_POST["Submit"])){
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Dashboard</title>
+    <title>Manage Admins</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <script src="js/jquery-3.2.1.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
@@ -65,10 +69,10 @@ if(isset($_POST["Submit"])){
                 <li><a href="AddNewPost.php">
                         <span class="glyphicon glyphicon-list-alt"></span>
                         &nbsp;Add New Post</a> </li>
-                <li class="active"><a href="Categories.php">
+                <li><a href="Categories.php">
                         <span class="glyphicon glyphicon-tags"></span>
                         &nbsp;Categories</a> </li>
-                <li><a href="#">
+                <li class="active"><a href="Admins.php">
                         <span class="glyphicon glyphicon-user"></span>
                         &nbsp;Manage Admin</a> </li>
                 <li><a href="#">
@@ -85,20 +89,28 @@ if(isset($_POST["Submit"])){
 
         </div> <!-- Ending of side srea -->
         <div class="col-sm-10">
-            <h1>Manage Categories</h1>
+            <h1>Manage Admin Access</h1>
             <?php
                 echo Message();
                 echo SuccessMessage();
             ?>
             <div>
-            <form action="Categories.php" method="post">
+            <form action="Admins.php" method="post">
                 <fieldset>
                     <div class="form-group">
-                        <label for="categoryname"><span class="FieldInfo">Name:</span></label>
-                        <input class="form-control" type="text" name="Category" id="categoryname" placeholder="Name">
+                        <label for="Username"><span class="FieldInfo">Username:</span></label>
+                        <input class="form-control" type="text" name="Username" id="Username" placeholder="Username">
+                    </div>
+                    <div class="form-group">
+                        <label for="Password"><span class="FieldInfo">Password:</span></label>
+                        <input class="form-control" type="Password" name="Password" id="Password" placeholder="Password">
+                    </div>
+                    <div class="form-group">
+                        <label for="ConfirmPassword"><span class="FieldInfo">Confirm Password:</span></label>
+                        <input class="form-control" type="Password" name="ConfirmPassword" id="ConfirmPassword" placeholder="Confirm Password">
                     </div>
                     <br>
-                    <input class="btn btn-primary btn-block" type="Submit" name="Submit" value="Add New Category">
+                    <input class="btn btn-primary btn-block" type="Submit" name="Submit" value="Add New Admin">
                     <br>
                 </fieldset>
 
@@ -109,29 +121,29 @@ if(isset($_POST["Submit"])){
                     <tr>
                         <th>Sr. No</th>
                         <th>Date & Time</th>
-                        <th>Category Name</th>
-                        <th>Creator Name</th>
+                        <th>Admins</th>
+                        <th>Added BY</th>
                         <th>Action</th>
                     </tr>
                     <?php
                         global $ConnectingDB;
-                        $ViewQuery = "SELECT * FROM category ORDER BY datetime DESC ";
+                        $ViewQuery = "SELECT * FROM registration ORDER BY datetime DESC ";
                         $Execute = mysql_query($ViewQuery);
                         $SrNo = 0;
                         while($DataRows = mysql_fetch_array($Execute)) {
                             $Id = $DataRows["id"];
                             $DateTime = $DataRows["datetime"];
-                            $CategoryName = $DataRows["name"];
-                            $CreatorName = $DataRows["creatorname"];
+                            $UserName = $DataRows["username"];
+                            $AddedBy = $DataRows["addedby"];
                             $SrNo++;
 
                     ?>
                     <tr>
                         <td><?php echo $SrNo; ?></td>
                         <td><?php echo $DateTime; ?></td>
-                        <td><?php echo $CategoryName; ?></td>
-                        <td><?php echo $CreatorName; ?></td>
-                        <td><a href="DeleteCategory.php?id=<?php echo $Id; ?>"><span class="btn btn-danger">Delete</span></a></td>
+                        <td><?php echo $UserName; ?></td>
+                        <td><?php echo $AddedBy; ?></td>
+                        <td><a href="DeleteAdmin.php?id=<?php echo $Id; ?>"><span class="btn btn-danger">Delete</span></a></td>
                     </tr>
                     <?php } ?>
                 </table>
